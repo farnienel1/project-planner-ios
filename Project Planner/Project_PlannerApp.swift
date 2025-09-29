90c15f319ad640e99744d77db418643dbd9bb4dc
farnienel1 <farnienel@hotmail.com>
2025-09-29 16:37:24 +0100

    Initial Commit

---
+++ Project Planner/Project_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_PlannerApp.swift
+//  Project Planner
+//
+//  Created by Farnie Nel on 29/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Project_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
